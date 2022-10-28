411cd6cdc0e0d0e4405fecc2abe60c60c56c52da
Will <wrightw404@gmail.com>
2022-10-28 01:44:38 -0700

    syntax refactor in pubDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,6 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
