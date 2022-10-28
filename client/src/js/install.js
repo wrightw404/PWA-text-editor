c7d0da2c88e8ef09eff0007b6b8e5c8189682ae5
Will <wrightw404@gmail.com>
2022-10-28 01:51:49 -0700

    added handler for appinstalled event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -19,4 +19,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
